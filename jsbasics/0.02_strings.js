022f9ae2c9187c9fa087c7397058e129e8b9e997
ccharris <ccharris27@gmail.com>
2016-09-21 11:19:41 -0400

    added string files

---
+++ jsbasics/0.02_strings.js
@@ -0,0 +1,31 @@
+// console.log("Hello World");
+// var birthCity = "I was born in Virginia Beach";
+// var birthState = "Virginia";
+// console.log(birthCity + ", " + birthState); //concatenation
+// console.log(birthCity, birthState);
+
+
+// var username = "ccharris";
+// var password = "123letmein";
+// var tweet = "kanye is broke again";
+
+// var facebookPost = "here are some pictures of us at the beach";
+// var instaCaption = "#awesome";
+
+// console.log(username);
+// console.log("                     ");
+
+var graduationYear = "2008";
+var highSchool = "Dreyfoos School of the Arts";
+var graduateText = "I graduated from ";
+var textIn = " in ";
+var period = "."
+var space = " "
+
+console.log(graduateText + highSchool + textIn + graduationYear + period);
+
+var carMake = "Volkswagon";
+var carModel = "Golf Sportwagen";
+var carYear = "2016";
+var carTotal = carMake + space + carModel + space + carYear
+console.log(carTotal);
