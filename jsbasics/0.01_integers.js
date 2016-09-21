efd39b4a69d0a935bef757b7cd93c114dce15978
ccharris <ccharris27@gmail.com>
2016-09-21 10:29:12 -0400

    added js folder and updated storeapp

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,58 @@
+// var num = 7;
+// var newNum = 5; //this is camel casing 
+// var newer_num = 10; //this is snake casing
+// var API_KEY = 547195820128481; //constant
+// var num_addition = 1 + 2
+// var num_adds = num + newNum
+
+// //console.log() prints to console
+// console.log(5 + 5);
+// //operations with console.log
+// console.log(num - newNum);
+// console.log(5 * 5);
+// console.log(5 / 1);
+
+// //modulus operator
+// console.log(10 % 5); //modulus operator - shows the remainder
+// console.log(11 % 5);
+
+// console.log(12 % 4);
+// console.log(15 % 4);
+// console.log(num_addition);
+// console.log(num_adds);
+
+// var newNum = 10;
+// newNum = newNum + 1;
+// console.log(newNum);
+// newNum++; //add 1 to a number, short cut for newNum = newNum + 1;
+// console.log(newNum);
+
+// newNum = newNum - 1;
+// console.log(newNum);
+// newNum--; //shortcut to subtract 1
+// console.log(newNum);
+
+// newNum = newNum + 10;
+// console.log(newNum);
+// newNum += 10; //shortcut for addition of any number to a variable
+// console.log(newNum);
+// newNum -= 20;
+// console.log(newNum);
+// newNum = newNum - 0;
+// console.log(newNum);
+// newNum *= 10;
+// console.log(newNum);
+// newNum /= 10;
+// console.log(newNum);
+
+
+
+
+
+
+
+
+
+
+
+
