37963801c0a045ae5f66b31de9966894ee8024db
ccharris <ccharris27@gmail.com>
2016-09-21 15:42:32 -0400

    added to img for storeapp

---
+++ jsbasics/0.08_function.js
@@ -0,0 +1,45 @@
+// function helloWorld() {
+// 	console.log("Hello World");
+// }
+// helloWorld();
+
+// function printMyName() {
+// 	console.log("Carolyn Harris")
+// }
+// printMyName();
+
+// var name = function() {
+// 	console.log("Carolyn")
+// }
+// name();
+
+// function addNumbers() {
+// 	var x = 10;
+// 	var y = 10;
+// 	console.log(x + y)
+// }
+
+// addNumbers();
+
+// function multipleTwoNumber() {
+// 	var x = 2;
+// 	var y = 4;
+// 	console.log(x * y)
+// }
+// multipleTwoNumber();
+
+// function printNumber(x){
+// 	console.log("The number you entered was: " + x);
+// }
+// printNumber(6);
+
+function bankPin(x){
+	cardPin = 45678;
+	if(x == cardPin) {
+	console.log(x + " is the correct pin.");
+} else {
+	console.log(x + " is not the correct pin.");
+}
+}
+bankPin(45678);
+
